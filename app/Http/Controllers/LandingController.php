6db9b4f004924ae0b95435cea3ad58581a03b659
AndiIskandarsyah <andiiskandarsyah923@gmail.com>
2023-10-26 13:53:33 +0700

    add landing dan controllernya

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
@@ -11,6 +12,9 @@ class LandingController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        //Ambil data artikel diurutkan berdasarkan tanggal terbaru. lalu tampilkan 7 artikel per halaman
+        $transactions = Transaction::query()->orderBy('created_at')->paginate(7);
+        //Ambil artikel pertama dari hasil query diatas
+        return view('landing', compact('transactions'));
     }
 }
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,40 +1,46 @@
 @extends('layouts.template')
 
-@section('title', 'Tentang Perpustakaan') <!-- Mengganti judul halaman -->
+@section('title', 'Tentang Transaksi')
 
 @section('content')
-    <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+    <div class="p-4 p-md-5 mb-4 text-white rounded bg-primary">
         <div class="row mb-2">
             <div class="col-md-6 px-0">
-                <h1 class="display-4 fst-italic">Selamat Datang di Perpustakaan</h1> <!-- Menambah judul selamat datang -->
-                <p class="lead my-3">Kami menyediakan akses ke beragam koleksi buku dan sumber daya bermanfaat.</p> <!-- Tambahkan deskripsi perpustakaan -->
+                <h1 class="display-4 fst-italic">Selamat Datang di Transaksi</h1>
+                <p class="lead my-3">Kami menyediakan informasi mengenai transaksi dan keuangan Anda.</p>
+
                 <p class="lead mb-0">
                     <a href="#" class="text-white fw-bold">
-                        Telusuri lebih lanjut...
+                        Pelajari lebih lanjut...
                     </a>
                 </p>
             </div>
             <div class="col-md-6">
-                <!-- Isi gambar atau konten visual yang relevan tentang perpustakaan -->
+
             </div>
         </div>
     </div>
 
-    {{-- Artikel Perpustakaan --}}
+    {{-- Informasi Transaksi --}}
     <div class="row mb-2">
-        <div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">Berita Terbaru</strong> <!-- Ganti kategori "World" dengan "Berita Terbaru" -->
-                    <h3 class="mb-0">Judul Artikel Terbaru</h3> <!-- Ganti judul artikel -->
-                    <div class="mb-1 text-muted">Tanggal Terbit</div> <!-- Ganti tanggal terbit -->
-                    <p class="card-text mb-auto">Deskripsi artikel singkat.</p> <!-- Ganti deskripsi artikel -->
-                    <a href="#" class="stretched-link">Baca selengkapnya</a> <!-- Ganti link "Continue reading" dengan "Baca selengkapnya" -->
-                </div>
-                <div class="col-auto d-none d-lg-block">
-                    <!-- Isi gambar artikel atau konten visual yang relevan -->
+        @forelse ($transactions as $transaction)
+            <div class="col-md-6">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-primary">{{ $transaction->type }}</strong>
+                        <h3 class="mb-0">{{ $transaction->category }}</h3>
+                        <div class="mb-1 text-muted">{{ $transaction->created_at }}</div>
+                        <div class="mb-1 text-muted">Rp. {{ number_format($transaction->amount) }}</div>
+                        <p class="card-text mb-auto">{{ Str::limit($transaction->notes, 50, ' ...') }}</p>
+                        <a href="{{ route('transactions.show', $transaction) }}" class="stretched-link">Continue reading</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        @empty
+            <p>No transactions available.</p>
+        @endforelse
+    </div>
+
+
     </div>
 @endsection
